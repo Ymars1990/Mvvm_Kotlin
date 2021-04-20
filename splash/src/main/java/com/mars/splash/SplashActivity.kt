9d49e0070f983b9e7d78c1cbcd253b6a6f747908
Ymars1990 <651717448@qq.com>
2021-04-20 14:39:30 +0800

    1、build.gradle plugin优化 2、全屏设置

--- splash/src/main/java/com/mars/splash/SplashActivity.kt
+++ splash/src/main/java/com/mars/splash/SplashActivity.kt
@@ -1,38 +1,54 @@
 package com.mars.splash
 
+import android.os.Build
 import android.os.Bundle
 import android.os.Message
 import android.view.View
+import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.bumptech.glide.Glide
-import com.mars.network.base.BaseReponseModel
 import com.mars.splash.SplashConstant.Companion.AD_DELAYMILLIS
 import com.mars.splash.bean.SplashAdBean
 import com.mars.splash.databinding.ActivitySplashBinding
 import com.mars.splash.model.SplashViewModel
-import com.ymars.poj.base.state.DataState
 import com.ymars.poj.base.ui.LifecycerActivity
 import com.ymars.poj.component.ArouterConstant
 import com.ymars.poj.comutils.LogTools
-import kotlinx.android.synthetic.main.activity_splash.*
 import kotlin.random.Random
 
 @Route(path = ArouterConstant.Splash_MAIN)
 class SplashActivity : LifecycerActivity<SplashViewModel, ActivitySplashBinding>() {
     override fun setLayout(savedInstanceState: Bundle?): Int {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         return R.layout.activity_splash
     }
 
     override fun initParams() {
+        hideSysbar()
         super.initParams()
         vb = DataBindingUtil.setContentView(this, R.layout.activity_splash)
     }
 
+    fun hideSysbar(){
+        if (Build.VERSION.SDK_INT >=30) {
+            val controller = ViewCompat.getWindowInsetsController(window.decorView)
+            controller?.also {
+                it.hide(WindowInsetsCompat.Type.statusBars())
+                it.hide(WindowInsetsCompat.Type.navigationBars())
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
     override fun doWork() {
         handler.sendEmptyMessage(0x01);
         vm.showAd()
@@ -57,10 +73,10 @@ class SplashActivity : LifecycerActivity<SplashViewModel, ActivitySplashBinding>
             }
         }
     }
+
     fun click(v: View?) {
         when (v?.id) {
-            R.id.adDuraTv ->
-            {
+            R.id.adDuraTv -> {
                 LogTools.i(TAG, "跳过广告，跳转主页")
                 handler.removeCallbacksAndMessages(null)
                 ARouter.getInstance().build(ArouterConstant.APP_MAIN).navigation()
@@ -68,6 +84,7 @@ class SplashActivity : LifecycerActivity<SplashViewModel, ActivitySplashBinding>
             }
         }
     }
+
     private val observer by lazy {
         Observer<SplashAdBean> {
             it?.let {
